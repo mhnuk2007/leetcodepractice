285be2953b651dec2a7432135921c26bab6f764c
Mohan Lal <mhnuk2007@gmail.com>
2025-04-19 01:06:21 +0500

    create MaxProfit.java

---
+++ src/MaxProfit.java
@@ -0,0 +1,18 @@
+public class MaxProfit {
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+
+
+
+        return maxProfit;
+    }
+
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+}
+
+
