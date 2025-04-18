5116060255e5465369599e4f15eedc949cc1587a
Mohan Lal <mhnuk2007@gmail.com>
2025-04-19 00:11:12 +0500

    Add Boyer-Moore MajorityElement solution using while loop

--- src/MajorityElement.java
+++ src/MajorityElement.java
@@ -1,20 +1,22 @@
 public class MajorityElement {
-    public static int majorityElement(int[] nums){
-        int count = 0, candidate =0;
+    public static int majorityElement(int[] nums) {
+        int count = 0, candidate = 0;
 
-        int i=0;
-        while (i<nums.length){
-            if (count == 0){
+        int i = 0;
+        while (i < nums.length) {
+            if (count == 0) {
                 candidate = nums[i];
-                count =1;
-        }
-            count+= (candidate==nums[i]) ? 1:-1;
+                count = 1;
+            } else {
+                count += (nums[i] == candidate) ? 1 : -1;
+            }
             i++;
         }
         return candidate;
     }
+
     public static void main(String[] args) {
-        int[] nums = {2, 2, 1, 1, 1, 2, 2};
-        System.out.println(majorityElement(nums));  // Output: 2
+        int[] nums = {6,5,5};
+        System.out.println(majorityElement(nums));
     }
 }
