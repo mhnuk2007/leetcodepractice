6de0ef32e544aa4a84be85a462b43560e5ffbbe6
Mohan Lal <mhnuk2007@gmail.com>
2025-04-24 20:20:20 +0500

    Add FirstOccuranceInAString

---
+++ src/FirstOccuranceInAString.java
@@ -0,0 +1,17 @@
+public class FirstOccuranceInAString {
+    public static int strStr(String haystack, String needle) {
+        int m = needle.length(), n = haystack.length();
+        for (int i = 0; i < n - m; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
+                return i;
+            }
+        }
+return -1;
+    }
+
+    public static void main(String[] args) {
+        String haystack = "sadbutsad";
+        String needle = "sad";
+        System.out.println(strStr(haystack, needle));
+    }
+}
