abee3d056f512113a1f4f034ed2a2902791f17a5
Mohan Lal <mhnuk2007@gmail.com>
2026-03-02 17:42:15 +0500

    Add solution to count maximum consecutive ones problem

--- src/arrays101/MaxConsecutiveOnes.java
+++ src/arrays101/MaxConsecutiveOnes.java
@@ -1,4 +1,24 @@
 package arrays101;
 
 public class MaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        for(int num : nums){
+            if (num == 1){
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+
+        }
+        return maxCount;
+
+    }
+    public static void main(String[] args) {
+        MaxConsecutiveOnes obj = new MaxConsecutiveOnes();
+        int[] arr = {1,1,0,1,1,1};
+        System.out.println(obj.findMaxConsecutiveOnes(arr));
+    }
 }
