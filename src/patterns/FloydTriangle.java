f9c7aff17a47d17008a7d5a3f4bfddbadd6f8a44
Mohan Lal <mhnuk2007@gmail.com>
2026-03-03 12:22:34 +0500

    Add floyd's triangle example of 0's and 1's with for loop

--- src/patterns/FloydTriangle.java
+++ src/patterns/FloydTriangle.java
@@ -5,16 +5,24 @@ public class FloydTriangle {
         int n = 4;
         int k = 1;
         for (int i = 0; i < n; i++) {
+            //Floyd's Triangle of positive integers
 //            int j = 0;
 //            while (j <= i) {
 //                System.out.print(k + "  ");
 //                j++;
 //                k++;
 //            }
+//            for(int j = 0; j <= i; j++){
+//                System.out.print(k + " ");
+//                k++;
+//            }
+            //Floyd's triangle of binary values (0 or 1)
+
             for(int j = 0; j <= i; j++){
-                System.out.print(k + " ");
+                System.out.print(k%2 + " ");
                 k++;
             }
+
             System.out.println();
         }
 
