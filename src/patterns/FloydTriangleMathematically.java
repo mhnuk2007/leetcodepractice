db17760b98d4942940130f30875d142aad68f78a
Mohan Lal <mhnuk2007@gmail.com>
2026-03-03 17:42:12 +0500

    Add Floyd Triangle using mathematical formula (triangular number approach)

---
+++ src/patterns/FloydTriangleMathematically.java
@@ -0,0 +1,13 @@
+package patterns;
+
+public class FloydTriangleMathematically {
+    public static void main(String[] args) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(((((i-1)*i)/2)+j) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
