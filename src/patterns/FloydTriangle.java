633d06e31d940f330583ccc916a887e1f69ee6c0
Mohan Lal <mhnuk2007@gmail.com>
2026-03-03 12:12:50 +0500

    Add floyd's triangle example with while loop

---
+++ src/patterns/FloydTriangle.java
@@ -0,0 +1,20 @@
+package patterns;
+
+public class FloydTriangle {
+    public static void main(String[] args) {
+        int n = 4;
+        int k = 1;
+        for (int i = 0; i < n; i++) {
+            int j = 0;
+            while (j <= i) {
+
+                System.out.print(k + "  ");
+
+                j++;
+                k++;
+            }
+            System.out.println();
+        }
+
+    }
+}
