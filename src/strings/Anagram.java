2ad88ae59d4bbb289d3c6622fda13ead067f4c7c
Mohan Lal <mhnuk2007@gmail.com>
2026-03-04 16:56:52 +0500

    update anagaram question

--- src/strings/Anagram.java
+++ src/strings/Anagram.java
@@ -9,27 +9,29 @@ public class Anagram {
         if (a.length() != b.length()) {
             return false;
         }
-        // Convert to char arrays
-        char[] arr1 = a.toLowerCase().toCharArray();
-        char[] arr2 = b.toLowerCase().toCharArray();
 
-        // Sort arrays (in-place)
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        // Compare sorted arrays
-        return Arrays.equals(arr1, arr2);
+        int[] freq = new int[26];
+        for(int i = 0; i < a.length(); i++){
+            freq[a.charAt(i) - 'a']++;
+            freq[b.charAt(i) - 'a']--;
+        }
+        for(int count : freq){
+            if(count != 0){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a string: ");
+        System.out.print("Enter a string: ");
         String a = scan.next();
-        System.out.println("Enter another string: ");
+        System.out.print("Enter another string: ");
         String b = scan.next();
         scan.close();
         boolean ret = isAnagram(a, b);
-        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
     }
 }
